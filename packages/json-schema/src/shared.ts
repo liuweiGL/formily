93526229ab8468d88dae73ab06343f18a8ca2e5a
liuwei <wei2460@qq.com>
2025-06-21 09:06:24 +0800

    fix(json-schema): isReactElement 判断有误

--- packages/json-schema/src/shared.ts
+++ packages/json-schema/src/shared.ts
@@ -1,15 +1,8 @@
-import {
-  isFn,
-  each,
-  isPlainObj,
-  isArr,
-  toArr,
-  FormPath,
-  isReactElement,
-} from '@formily/shared'
+import { isFn, each, isPlainObj, isArr, toArr, FormPath } from '@formily/shared'
 import { isObservable, untracked } from '@formily/reactive'
 import { Schema } from './schema'
 import { ISchema } from './types'
+import { isValidElement } from 'react'
 
 const REVA_ACTIONS_KEY = Symbol.for('__REVA_ACTIONS')
 
@@ -158,7 +151,7 @@ export const traverseSchema = (
 }
 
 export const isNoNeedCompileObject = (source: any) => {
-  if (isReactElement(source)) {
+  if (isValidElement(source)) {
     return true
   }
   if (source['_isAMomentObject']) {
